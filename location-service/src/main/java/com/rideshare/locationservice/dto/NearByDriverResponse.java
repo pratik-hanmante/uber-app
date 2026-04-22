b432251be815511e731260f0040f05223e46f259
pratik <pratikhanmante3@gmail.com>
2026-04-22 23:37:07 +0530

    near by driver nearbyDriverResponse

--- location-service/src/main/java/com/rideshare/locationservice/dto/NearByDriverResponse.java
+++ location-service/src/main/java/com/rideshare/locationservice/dto/NearByDriverResponse.java
@@ -1,6 +1,9 @@
 package com.rideshare.locationservice.dto;
 
 public class NearByDriverResponse {
-
+        private String driverId;
+        private String latitude;
+        private String longitude;
+        private String distanceInKm;
 
 }
